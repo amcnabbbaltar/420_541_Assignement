fdb00b22efe93d976e66c65615ae6e7407365733
amcnabbbaltar <123915347+amcnabbbaltar@users.noreply.github.com>
2025-02-05 00:22:01 -0500

    Update Assignement

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class AnimatorController : MonoBehaviour
 {
     private Animator animator;
-    private CharacterMovement movement;
+    private CharacterMovement characterMovement;
     public void Start()
     {
         animator = GetComponent<Animator>();
-        movement = GetComponent<CharacterMovement>();
+        characterMovement = GetComponent<CharacterMovement>();
     }
     public void LateUpdate()
     {
